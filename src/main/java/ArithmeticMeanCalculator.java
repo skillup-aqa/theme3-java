d8a48830d6cf11db027a37c219c5e950727386f5
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-03-02 13:51:08 +0100

    Adding github actions on push

--- src/main/java/ArithmeticMeanCalculator.java
+++ src/main/java/ArithmeticMeanCalculator.java
@@ -1,5 +1,5 @@
 public class ArithmeticMeanCalculator {
-    public static double calculateArithmeticMean(double a, double b, double c) {
-        return -1;
-    }
+  public static double calculateArithmeticMean(double a, double b, double c) {
+    return -1;
+  }
 }
