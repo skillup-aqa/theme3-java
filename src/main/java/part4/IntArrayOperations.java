72f61dbb5555f7c94c8a7c237efdd84ffd21d231
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-03-03 15:15:37 +0100

    Adding classes and test for part 4

---
+++ src/main/java/part4/IntArrayOperations.java
@@ -0,0 +1,51 @@
+package part4;
+
+public class IntArrayOperations {
+    public static int[] fillWithNumbers(int[] array) {
+        return null;
+    }
+
+    public static int max(int[] array) {
+        return -1;
+    }
+
+    public static int min(int[] array) {
+        return -1;
+    }
+
+
+    public static int sum(int[] array) {
+        return -1;
+    }
+    public static int avg(int[] array) {
+        return -1;
+    }
+
+    public static int[] reverse(int[] array) {
+        return null;
+    }
+
+    public static int[] copy(int[] array) {
+        return null;
+    }
+
+    public static int indexOf(int[] array, int value) {
+        return -1;
+    }
+
+    public static int[] removeDuplicates(int[] array) {
+        return null;
+    }
+
+    public static int[] sort(int[] array) {
+        return null;
+    }
+
+    public static int[] merge(int[] array1, int[] array2) {
+        return null;
+    }
+
+    public static int binarySearch (int[] array, int value) {
+        return -1;
+    }
+}
