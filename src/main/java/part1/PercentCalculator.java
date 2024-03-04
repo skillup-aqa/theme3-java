74f477689458a4b165890778d9710f935214111f
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-04 18:53:15 +0200

    calculatePercent resolving also added calculation from example 8% from 50

--- src/main/java/part1/PercentCalculator.java
+++ src/main/java/part1/PercentCalculator.java
@@ -2,14 +2,18 @@ package part1;
 
 public class PercentCalculator {
     public static double calculatePercent(double number, double percent) {
-        // TODO: Implement solution here
-        return -1;
+        double calculation = (percent / 100) * number;
+        return calculation;
     }
 
     public static void main(String[] args) {
         double number = 100;
         double percent = 10;
         double result = calculatePercent(number, percent);
+        System.out.println(result);
+        number = 50;
+        percent = 8;
+        result = calculatePercent(number, percent);
         System.out.println(result);
     }
 }
