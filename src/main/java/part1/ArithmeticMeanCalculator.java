4cdf8b1de6c62ec9c7176f8ef901b50cf5ecad2a
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-05 09:36:21 +0200

    ArithmeticMeanCalculator resolving Code formatting is applied in accordance with comment in PR

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -2,7 +2,7 @@ package part1;
 
 public class ArithmeticMeanCalculator {
     public static double calculateArithmeticMean(double a, double b, double c) {
-        return (a + b + c)/3;
+        return (a + b + c) / 3;
     }
 
     public static void main(String[] args) {
