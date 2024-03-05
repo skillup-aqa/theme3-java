49821e785f98178189087c7258162cda925bec56
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-05 21:24:30 +0200

    SalaryCalculator resolving Deleted currency rates, applied final to eSV and percent in accordance with comment in PR

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -5,19 +5,15 @@ import static java.lang.Math.ceil;
 public class SalaryCalculator {
     public static double calculateSalaryBeforeTaxes(double hoursPerMonth, double hourlyRate) {
         // TODO: Implement solution here
-        double currencyRateUSD = 38.1575;
-        double hourlyRateHRN = hourlyRate * currencyRateUSD;
-        double resultBT = hoursPerMonth * hourlyRateHRN;
+        double resultBT = hoursPerMonth * hourlyRate;
         return resultBT;
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
-        double currencyRateUSD = 38.1575;
-        double hourlyRateHRN = hourlyRate * currencyRateUSD;
-        double resultBT = hoursPerMonth * hourlyRateHRN;
-        double eSV = 1562.0;
-        double percent = 0.05;
-        double resultAT = ceil((resultBT - resultBT*percent - eSV)*100)/100;
+        double resultBT = hoursPerMonth * hourlyRate;
+        final double eSV = 1562.0;
+        final double percent = 0.05;
+        double resultAT = ceil((resultBT - resultBT * percent - eSV) * 100) / 100;
         return resultAT;
     }
 
