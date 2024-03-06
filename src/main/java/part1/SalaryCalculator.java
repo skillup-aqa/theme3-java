50423bda53dad60d65adf2728434e6af2b394002
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-03-06 21:04:15 +0100

    Fixing the keys

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -7,7 +7,7 @@ public class SalaryCalculator {
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
         double salaryAfterTaxes = calculateSalaryBeforeTaxes(hoursPerMonth, hourlyRate) * 0.95 - 1562;
-        return Math.round(salaryAfterTaxes * 100) / 100.0;
+        return Math.ceil(salaryAfterTaxes * 100) / 100.0;
     }
 
     public static void main(String[] args) {
