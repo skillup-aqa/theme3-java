aa63b899339c9e658be3a30a7bfe3bb829e8b7b4
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-06 19:26:18 -0500

    Percent Calculator fix

--- src/main/java/part1/PercentCalculator.java
+++ src/main/java/part1/PercentCalculator.java
@@ -2,10 +2,8 @@ package part1;
 
 public class PercentCalculator {
     public static double calculatePercent(double number, double percent) {
-
         return (number*percent)/100;
     }
-
     public static void main(String[] args) {
         double number = 50;
         double percent = 8;
