473531b60160b3b1946a139a748e13ff591816ac
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-06 19:30:30 -0500

    DivisionWithRemainderCalculator fix

--- src/main/java/part1/DivisionWithRemainderCalculator.java
+++ src/main/java/part1/DivisionWithRemainderCalculator.java
@@ -4,7 +4,6 @@ public class DivisionWithRemainderCalculator {
     public static int calculateRemainder(int dividend, int divisor) {
         return (dividend%divisor);
     }
-
     public static void main(String[] args) {
         int dividend =10;
         int divisor = 3;
