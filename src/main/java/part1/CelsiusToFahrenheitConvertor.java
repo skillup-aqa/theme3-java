007bdf3e6c53b3a1adf1524bd17401e48877ea7c
Alina Wayne <malinka2012demochko@gmail.com>
2024-03-08 01:03:15 -0500

    Changes to main block 1 Implementing solution for calculateArithmeticMean Implementing C to F ( C = 0)

--- src/main/java/part1/CelsiusToFahrenheitConvertor.java
+++ src/main/java/part1/CelsiusToFahrenheitConvertor.java
@@ -1,12 +1,15 @@
-package part1;
-
-public class CelsiusToFahrenheitConvertor {
-    public static double convertCelsiusToFahrenheit(double celsius) {
-        // TODO: Implement solution here
-        return -1;
-    }
+public class ConvertCelsiusToFahrenheit {
 
     public static void main(String[] args) {
+        double C1 = 0;
+        int a = 0 * 9 / 5;
+        int b = a + 32;
+        double F1 = b;
+        System.out.println(F1);
+                
+             
+        
+        
         System.out.println(convertCelsiusToFahrenheit(0)); // Expected output: 32.0
         System.out.println(convertCelsiusToFahrenheit(100)); // Expected output: 212.0
         System.out.println(convertCelsiusToFahrenheit(37)); // Expected output: 98.6
