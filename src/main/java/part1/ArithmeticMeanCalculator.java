005ac2db7fa068ede348edf280d71777e25e4726
Alina Wayne <malinka2012demochko@gmail.com>
2024-03-07 23:30:03 -0500

    Changes to main block

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -1,16 +1,15 @@
 package part1;
 
 public class ArithmeticMeanCalculator {
-    public static double calculateArithmeticMean(double a, double b, double c) {
-        // TODO: Implement solution here
-        return -1;
+
+
     }
 
     public static void main(String[] args) {
-        double a = 1;
-        double b = 2;
-        double c = 3;
-        double result = calculateArithmeticMean(a, b, c);
+        int double a = 1;
+        int double b = 2;
+        int double c = 3;
+        double result = calculateArithmeticMean(a, b, c) / 3;
         System.out.println(result);
     }
 }
