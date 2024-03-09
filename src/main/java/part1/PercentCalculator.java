b6f292e7e73a52f3beb98248f0ecf17a37ce0bbd
Alina Wayne <malinka2012demochko@gmail.com>
2024-03-09 01:36:36 -0500

    Changes to main block 1 Implementing solution for calculateArithmeticMean Implementing C1 to F1 ( C = 0) Implementing C2 to F2 ( C = 100) Implementing C3 to F3 ( C = 0) Implementing division program Implementing HoursToMinutesAndSecondsConvertor Implementing PercentCalculator

--- src/main/java/part1/PercentCalculator.java
+++ src/main/java/part1/PercentCalculator.java
@@ -1,15 +1,9 @@
-package part1;
-
 public class PercentCalculator {
-    public static double calculatePercent(double number, double percent) {
-        // TODO: Implement solution here
-        return -1;
-    }
-
+ 
     public static void main(String[] args) {
-        double number = 100;
-        double percent = 10;
-        double result = calculatePercent(number, percent);
-        System.out.println(result);
+        double total = 100;
+        double value = 10;
+        double percentage = (value / total) * 100;
+        System.out.println(result(percentage));
     }
 }
