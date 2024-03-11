385f1a30b0fa4da9521d2bd209400221354ed2e3
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-11 15:41:21 +0200

    LeapYearChecker resolving

--- src/main/java/part2/LeapYearChecker.java
+++ src/main/java/part2/LeapYearChecker.java
@@ -4,7 +4,13 @@ public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
         // Read about leap years here: https://en.wikipedia.org/wiki/Leap_year
         // TODO: Implement solution here
-        return false;
+        boolean isLeapYear;
+        if (year % 100 == 0 && year % 400 != 0) {
+            isLeapYear = false;
+        } else {
+            isLeapYear = year % 4 == 0;
+        }
+        return isLeapYear;
     }
 
     public static void main(String[] args) {
