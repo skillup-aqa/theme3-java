8a437b25423fbeb41e0c3e9b28872dc87f72c234
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-12 08:00:17 -0400

    Changes after comments

--- src/main/java/part1/HoursToMinutesConvertor.java
+++ src/main/java/part1/HoursToMinutesConvertor.java
@@ -2,11 +2,9 @@ package part1;
 
 public class HoursToMinutesConvertor {
     public static int convertHoursToMinutes(int hours) {
-        // TODO: Implement solution here
-        return (hours*60);
+        return hours*60;
     }
     public static int convertHoursToSeconds(int hours) {
-        // TODO: Implement solution here
         return (hours*60)*60;
     }
     public static void main(String[] args) {
