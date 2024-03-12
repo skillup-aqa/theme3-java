b13d4c330ce9839b547c9119fda5d08481955fb4
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-12 18:15:05 +0200

    convertPointsToMarks resolving

--- src/main/java/part2/PointsToMarksConvertor.java
+++ src/main/java/part2/PointsToMarksConvertor.java
@@ -3,8 +3,15 @@ package part2;
 public class PointsToMarksConvertor {
     public static char convertPointsToMarks(int points) {
         // Assume that points are always non-negative integers and less than 100
-        // TODO: Implement solution here
-        return 'X';
+        char mark;
+
+        if (points < 60) mark = 'F';
+        else if (points <= 74) mark = 'D';
+        else if (points <= 84) mark = 'C';
+        else if (points <= 89) mark = 'B';
+        else mark = 'A';
+
+        return mark;
     }
 
     public static void main(String[] args) {
