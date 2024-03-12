2483b9bd9b85aa224000c7756250e01cb4b89500
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-12 08:08:11 -0400

    Changes after comments

--- src/main/java/part1/DivisionWithRemainderCalculator.java
+++ src/main/java/part1/DivisionWithRemainderCalculator.java
@@ -2,7 +2,7 @@ package part1;
 
 public class DivisionWithRemainderCalculator {
     public static int calculateRemainder(int dividend, int divisor) {
-        return (dividend%divisor);
+        return dividend % divisor;
     }
     public static void main(String[] args) {
         int dividend =10;
