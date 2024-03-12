d94dffb6039b2324c6f8e0cc3ee001cebab84de8
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-12 08:07:08 -0400

    Changes after comments

--- src/main/java/part1/PercentCalculator.java
+++ src/main/java/part1/PercentCalculator.java
@@ -1,7 +1,8 @@
 package part1;
 public class PercentCalculator {
     public static double calculatePercent(double number, double percent) {
-        return (number*percent)/100;
+
+        return  number * percent / 100;
     }
     public static void main(String[] args) {
         double number = 50;
