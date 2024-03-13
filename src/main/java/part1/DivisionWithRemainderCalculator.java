eee864c1997d25164d358aeebab9d23497f45501
Anna-Mariia Duliba <annamariya10011@gmail.com>
2024-03-13 11:04:10 -0700

    added solution for DivisionWithRemainderCalculator

--- src/main/java/part1/DivisionWithRemainderCalculator.java
+++ src/main/java/part1/DivisionWithRemainderCalculator.java
@@ -2,13 +2,14 @@ package part1;
 
 public class DivisionWithRemainderCalculator {
     public static int calculateRemainder(int dividend, int divisor) {
-        // TODO: Implement solution here
-        return -1;
+
+        int calculateRemainder = dividend / divisor ;
+        return calculateRemainder;
     }
 
     public static void main(String[] args) {
         int dividend = 10;
-        int divisor = 3;
+        int divisor = 2;
         int result = calculateRemainder(dividend, divisor);
         System.out.println(result);
     }
