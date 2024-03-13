8a0b76096040ca59ae2633798353c85cd036e27d
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-13 17:27:24 +0200

    AgeOnPlanetCalculator resolving

--- src/main/java/part2/AgeOnPlanetCalculator.java
+++ src/main/java/part2/AgeOnPlanetCalculator.java
@@ -3,8 +3,38 @@ package part2;
 public class AgeOnPlanetCalculator {
     public static double calculateAgeOnPlanet(int ageInYears, String planet) {
         // If the planet is not in the list, return -1
-        // TODO: Implement solution here
-        return -1;
+        double doubleAgeInYears = ageInYears;
+        double age;
+        switch (planet) {
+            case "Mercury":
+                age = doubleAgeInYears / 0.2408467;
+                break;
+            case "Venus":
+                age = doubleAgeInYears / 0.61519726;
+                break;
+          /*  case "Earth":
+                age = doubleAgeInYears;
+                break; */
+            case "Mars":
+                age = doubleAgeInYears / 1.8808158;
+                break;
+            case "Jupiter":
+                age = doubleAgeInYears / 11.862615;
+                break;
+            case "Saturn":
+                age = doubleAgeInYears / 29.447498;
+                break;
+            case "Uranus":
+                age = doubleAgeInYears / 84.016846;
+                break;
+            case "Neptune":
+                age = doubleAgeInYears / 164.79132;
+                break;
+            default:
+                age = -1;
+        }
+
+        return age;
     }
 
     public static void main(String[] args) {
