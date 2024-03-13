a4acee95138b65dbc20da28904fada4242a29b4b
Ivan Senkiv <senkvvan@gmail.com>
2024-03-13 18:46:59 +0000

    "Second fix for PercentCalculator"

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -7,12 +7,12 @@ public class SalaryCalculator {
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
-        double SalaryAfterTaxes = Math.ceil((hoursPerMonth * hourlyRate) - ((hoursPerMonth * hourlyRate) * 0.05)) - 1562.0;
-        return SalaryAfterTaxes;
+        double SalaryAfterTaxes = (((hoursPerMonth * hourlyRate) - ((hoursPerMonth * hourlyRate) * 0.05)) - 1562.0);
+        return Math.round((SalaryAfterTaxes * 100) / 100);
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(160, 10));
+        System.out.println(calculateSalaryBeforeTaxes(160.00, 10));
         System.out.println(calculateSalaryAfterTaxes(160, 10));
     }
 }
