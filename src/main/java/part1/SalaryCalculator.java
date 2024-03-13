00d3ca78336b33d4eee715c1f85c8a192488c9b1
Ivan Senkiv <senkvvan@gmail.com>
2024-03-13 19:48:43 +0000

    "Second fix for SalaryCalculator "

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -7,12 +7,12 @@ public class SalaryCalculator {
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
-        double SalaryAfterTaxes = (((hoursPerMonth * hourlyRate) - ((hoursPerMonth * hourlyRate) * 0.05)) - 1562.0);
-        return Math.round((SalaryAfterTaxes * 100) / 100);
+        double salaryAfterTaxes = calculateSalaryBeforeTaxes(hoursPerMonth, hourlyRate) * 0.95 - 1562;
+        return Math.ceil(salaryAfterTaxes * 100) / 100.0;
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(160.00, 10));
+        System.out.println(calculateSalaryBeforeTaxes(160, 10));
         System.out.println(calculateSalaryAfterTaxes(160, 10));
     }
 }
