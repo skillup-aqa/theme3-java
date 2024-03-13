4606408eeb062cc9398d258453f385de02a64928
Anna-Mariia Duliba <annamariya10011@gmail.com>
2024-03-13 12:16:59 -0700

    added solution for SalaryCalculator

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -2,17 +2,22 @@ package part1;
 
 public class SalaryCalculator {
     public static double calculateSalaryBeforeTaxes(double hoursPerMonth, double hourlyRate) {
-        // TODO: Implement solution here
-        return -1;
+        return hoursPerMonth * hourlyRate;
+
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
-        // TODO: Implement solution here
-        return -1;
+
+        double rateTax = (hoursPerMonth * hourlyRate ) * 0.05 + 1562;
+        double rate = hoursPerMonth * hourlyRate;
+        return Math.round(rate - rateTax);
+
+
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(160, 10));
-        System.out.println(calculateSalaryAfterTaxes(160, 10));
+        System.out.println(calculateSalaryBeforeTaxes(160, 100));
+        System.out.println(calculateSalaryAfterTaxes(160, 100));
+
     }
 }
