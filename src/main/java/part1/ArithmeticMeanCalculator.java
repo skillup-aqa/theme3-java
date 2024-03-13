898f7ae3925d575f46efaa68982c2992b81b03e2
Anna-Mariia Duliba <annamariya10011@gmail.com>
2024-03-13 11:30:42 -0700

    added solution for ArithmeticMeanCalculator

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -1,13 +1,15 @@
 package part1;
 
 public class ArithmeticMeanCalculator {
+    public static double calculateArithmeticMean(double a, double b, double c) {
+        return (a + b + c) / 3;
+    }
 
     public static void main(String[] args) {
-        double a = 3;
-        double b = 17;
-        double c = 45;
-        double middle = (a + b + c) / 3;
-
-        System.out.println(" middle number is " + middle);
+        double a = 1;
+        double b = 2;
+        double c = 3;
+        double result = calculateArithmeticMean(a, b, c);
+        System.out.println(result);
     }
 }
