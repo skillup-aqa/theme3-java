54663972883d8e08f1855c38f154fa8dab112b2f
Anna-Mariia Duliba <annamariya10011@gmail.com>
2024-03-12 17:42:53 -0700

    add 1st solution

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -1,10 +1,6 @@
 package part1;
 
 public class ArithmeticMeanCalculator {
-    public static double calculateArithmeticMean(double a, double b, double c) {
-        // TODO: Implement solution here
-        return -1;
-    }
 
     public static void main(String[] args) {
         double a = 3;
