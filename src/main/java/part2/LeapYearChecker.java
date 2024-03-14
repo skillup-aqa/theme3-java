82462193c368cdc6bfbc91b6f18fb226097d0062
Ivan Senkiv <senkvvan@gmail.com>
2024-03-14 20:59:19 +0000

    "Done for LeapYearChecker "

--- src/main/java/part2/LeapYearChecker.java
+++ src/main/java/part2/LeapYearChecker.java
@@ -3,8 +3,15 @@ package part2;
 public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
         // Read about leap years here: https://en.wikipedia.org/wiki/Leap_year
-        // TODO: Implement solution here
-        return false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
