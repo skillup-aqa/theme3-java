d9cc5516266194041a0d2871f56f5daec54b51f3
Dima <korio455455@gmail.com>
2024-03-14 16:37:48 -0500

    Change Celsiusto Fahrenheit

--- src/main/java/part1/CelsiusToFahrenheitConvertor.java
+++ src/main/java/part1/CelsiusToFahrenheitConvertor.java
@@ -1,14 +1,23 @@
 package part1;
-
+import java.util.Scanner;
 public class CelsiusToFahrenheitConvertor {
     public static double convertCelsiusToFahrenheit(double celsius) {
-        // TODO: Implement solution here
-        return -1;
+Scanner scan = new Scanner(System.in);
+        System.out.println("Enter C");
+        double x = scan.nextDouble();
+        int a = 9;
+        int b = 5;
+        int c = 32;
+
+        double F = (x * a / b) + c;
+        System.out.println(F);
+        return F;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(convertCelsiusToFahrenheit(0)); // Expected output: 32.0
-        System.out.println(convertCelsiusToFahrenheit(100)); // Expected output: 212.0
-        System.out.println(convertCelsiusToFahrenheit(37)); // Expected output: 98.6
+   System.out.println(convertCelsiusToFahrenheit(0)); // Expected output: 32.0
+    System.out.println(convertCelsiusToFahrenheit(100)); // Expected output: 212.0
+ System.out.println(convertCelsiusToFahrenheit(37)); // Expected output: 98.6
     }
 }
