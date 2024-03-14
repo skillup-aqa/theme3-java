e4dc7d8f381f8e4bb8a442f11256c84201b4d56f
Dima <korio455455@gmail.com>
2024-03-14 16:59:16 -0500

    Change ArithmeticMean

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -2,14 +2,18 @@ package part1;
 
 public class ArithmeticMeanCalculator {
     public static double calculateArithmeticMean(double a, double b, double c) {
-        // TODO: Implement solution here
-        return -1;
+        double x = 20;
+        double y = 20;
+        double z = 50;
+        double sum = (x+y+z)/3;
+        return sum;
+        //System.out.println(sum);
     }
 
     public static void main(String[] args) {
-        double a = 1;
-        double b = 2;
-        double c = 3;
+        double a = 10;
+        double b = 20;
+        double c = 30;
         double result = calculateArithmeticMean(a, b, c);
         System.out.println(result);
     }
