ec7da462d96a335ccab49dce64198e6c89e0a10d
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-14 20:35:25 -0400

    Solution-for-Arithmetic

--- src/main/java/part1/ArithmeticMeanCalculator.java
+++ src/main/java/part1/ArithmeticMeanCalculator.java
@@ -4,6 +4,7 @@ public class ArithmeticMeanCalculator {
     public static double calculateArithmeticMean(double a, double b, double c) {
         return (a + b + c) / 3.0;
     }
+
     public static void main(String[] args) {
         double result = calculateArithmeticMean(9.0, 2.0, 3.0);
 
