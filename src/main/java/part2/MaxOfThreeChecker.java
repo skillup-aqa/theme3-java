355f14911da1209eee1b71f238b871a460db411c
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-15 09:13:15 -0400

    Solution-for-VowelChecker

--- src/main/java/part2/MaxOfThreeChecker.java
+++ src/main/java/part2/MaxOfThreeChecker.java
@@ -2,8 +2,13 @@ package part2;
 
 public class MaxOfThreeChecker {
     public static int maxOfThree(int number1, int number2, int number3) {
-        // TODO: Implement solution here
-        return -1;
+        if (number1 > number2 && number1 > number3){
+            return number1;
+    } else if (number2 > number3 && number2 > number1){
+            return number2;
+        } else {
+            return number3;
+        }
     }
 
     public static void main(String[] args) {
