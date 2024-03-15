524a4d9c2f888d135cd56adf1d9a17eae1f1208e
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-15 09:27:54 -0400

    Solution-for-MaxOfThreeChecker

--- src/main/java/part2/MaxOfThreeChecker.java
+++ src/main/java/part2/MaxOfThreeChecker.java
@@ -2,9 +2,9 @@ package part2;
 
 public class MaxOfThreeChecker {
     public static int maxOfThree(int number1, int number2, int number3) {
-        if (number1 > number2 && number1 > number3){
+        if (number1 > number2 && number1 > number3) {
             return number1;
-    } else if (number2 > number3 && number2 > number1){
+        } else if (number2 > number3 && number2 > number1) {
             return number2;
         } else {
             return number3;
