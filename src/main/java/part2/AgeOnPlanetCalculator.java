8622e95a5fbc2ea04c85c1ac8eed504f5b8ebad2
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-15 14:47:33 -0400

    Solution-For-AgeOnPlanetCalculator

--- src/main/java/part2/AgeOnPlanetCalculator.java
+++ src/main/java/part2/AgeOnPlanetCalculator.java
@@ -3,8 +3,34 @@ package part2;
 public class AgeOnPlanetCalculator {
     public static double calculateAgeOnPlanet(int ageInYears, String planet) {
         // If the planet is not in the list, return -1
-        // TODO: Implement solution here
-        return -1;
+        double orbitalPeriod;
+
+        switch (planet.toLowerCase()) {
+            case "mercury":
+                orbitalPeriod = 0.2408467;
+                break;
+            case "venus":
+                orbitalPeriod = 0.61519726;
+                break;
+            case "mars":
+                orbitalPeriod = 1.8808158;
+                break;
+            case "jupiter":
+                orbitalPeriod = 11.862615;
+                break;
+            case "saturn":
+                orbitalPeriod = 29.447498;
+                break;
+            case "uranus":
+                orbitalPeriod = 84.016846;
+                break;
+            case "neptune":
+                orbitalPeriod = 164.79132;
+                break;
+            default:
+                return -1;
+        }
+        return ageInYears / orbitalPeriod;
     }
 
     public static void main(String[] args) {
