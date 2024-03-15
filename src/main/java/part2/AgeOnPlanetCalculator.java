a373a37cd57e5fd8a5c43b2cad72e0710320c339
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-15 14:49:02 -0400

    Solution-For-AgeOnPlanetCalculator

--- src/main/java/part2/AgeOnPlanetCalculator.java
+++ src/main/java/part2/AgeOnPlanetCalculator.java
@@ -2,7 +2,7 @@ package part2;
 
 public class AgeOnPlanetCalculator {
     public static double calculateAgeOnPlanet(int ageInYears, String planet) {
-        // If the planet is not in the list, return -1
+
         double orbitalPeriod;
 
         switch (planet.toLowerCase()) {
