35397bb2782bf74cbf19d5eeba0935d1e5964996
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-14 21:38:24 -0400

    Solution-for-EvenChecker

--- src/main/java/part2/EvenChecker.java
+++ src/main/java/part2/EvenChecker.java
@@ -3,8 +3,12 @@ package part2;
 public class EvenChecker {
     public static boolean isEven(int number) {
         // For sake of simplicity, we assume that 0 is an even number
-        // TODO: Implement solution here
-        return false;
+        boolean result = false;
+
+        if (number % 2 == 0) {
+            result = true;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
