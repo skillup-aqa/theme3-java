4da411bb01bd0e84bb4c5182e1d77da8d1b66c9c
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-15 10:34:48 -0400

    Solution-for-Temperature-Convertor

--- src/main/java/part2/TemperatureConvertor.java
+++ src/main/java/part2/TemperatureConvertor.java
@@ -4,17 +4,32 @@ import java.util.Scanner;
 
 public class TemperatureConvertor {
     public static double convertCelsiusToFahrenheit(double celsius) {
-        // TODO: Implement solution here
-        return -1;
+        return (celsius * 9.0 / 5.0) + 32;
     }
 
     public static double convertFahrenheitToCelsius(double fahrenheit) {
-        // TODO: Implement solution here
-        return -1;
+        return (5.0 / 9.0) * (fahrenheit - 32);
     }
 
     public static void requestInputAndCalculate() {
-        // TODO: Put here the request for temperature and scale
+        Scanner customInput = new Scanner(System.in);
+        System.out.println("Enter the temperature value;");
+        double temperature = customInput.nextDouble();
+
+        System.out.println("Enter the scale (C for Celsius, F for Fahrenheit):");
+        char scale = customInput.next().charAt(0);
+
+        if (scale == 'C' || scale == 'c') {
+            double fahrenheit = convertCelsiusToFahrenheit(temperature);
+            System.out.println("Temperature in Fahrenheit:" + fahrenheit);
+        } else if (scale == 'F' || scale == 'f') {
+            double celsius = convertFahrenheitToCelsius(temperature);
+            System.out.println("Temperature in Celsius:" + celsius);
+        } else {
+            System.out.println("Enter correct scale");
+        }
+
+        customInput.close();
     }
 
     public static void main(String[] args) {
