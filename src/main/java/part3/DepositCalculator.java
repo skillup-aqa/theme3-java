4f4501ac8cd84a640465367924df8f76147546a8
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-15 20:12:50 +0200

    DepositCalculator resolving

--- src/main/java/part3/DepositCalculator.java
+++ src/main/java/part3/DepositCalculator.java
@@ -2,7 +2,14 @@ package part3;
 
 public class DepositCalculator {
     public static void printFutureValue(double principal, double rate, int years) {
-        // TODO: Implement solution here
+        int i = 1;
+        double a = principal * (1 + rate);
+        System.out.println(i + " year Deposit is: " + a);
+        while (i < years) {
+            a = a * (1 + rate);
+            i++;
+            System.out.println(i + " year Deposit is: " + a);
+        }
     }
 
     public static void main(String[] args) {
