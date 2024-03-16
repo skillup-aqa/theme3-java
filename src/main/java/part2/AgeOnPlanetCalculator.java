e6bc0a937f9d8950c7a0da5473693867161ae203
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-16 11:56:53 -0400

    Resolving HW part 2

--- src/main/java/part2/AgeOnPlanetCalculator.java
+++ src/main/java/part2/AgeOnPlanetCalculator.java
@@ -3,13 +3,30 @@ package part2;
 public class AgeOnPlanetCalculator {
     public static double calculateAgeOnPlanet(int ageInYears, String planet) {
         // If the planet is not in the list, return -1
-        // TODO: Implement solution here
-        return -1;
+        switch (planet) {
+            case "Mercury":
+                return ageInYears / 0.2408467;
+            case "Venus":
+                return ageInYears / 0.61519726;
+            case "Mars":
+                return ageInYears / 1.8808158;
+            case "Jupiter":
+                return ageInYears / 11.862615;
+            case "Saturn":
+                return ageInYears / 29.447498;
+            case "Uranus":
+                return ageInYears / 84.016846;
+            case "Neptune":
+                return ageInYears / 164.79132;
+            default:
+                System.out.println("Unknown planet: " + planet);
+                 return - 1;
+        }
     }
 
     public static void main(String[] args) {
-        int ageInYears = 1;
-        String planet = "Mars";
+        int ageInYears = 20;
+        String planet = "Neptune";
         double result = calculateAgeOnPlanet(ageInYears, planet);
         System.out.println(result);
     }
--- src/main/java/part2/DartsGame.java
+++ src/main/java/part2/DartsGame.java
@@ -2,13 +2,20 @@ package part2;
 
 public class DartsGame {
     public static int getScore(double x, double y) {
-        // TODO: Implement solution here
-        return -1;
-    }
-
+        double radius = Math.sqrt(x * x + y * y);
+       if (radius <= 1) {
+           return 10;
+       }else if (radius <= 5) {
+           return 5;
+       }else if (radius <= 10) {
+           return 1;
+       }else {
+           return 0;
+       }
+}
     public static void main(String[] args) {
-        double x = 1.0;
-        double y = 1.0;
+        double x = 15.0;
+        double y = 15.0;
         int result = getScore(x, y);
         System.out.println(result);
     }
