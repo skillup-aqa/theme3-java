000ec4eb6ce2b4301e6f8b98aaabc75788bc0b8a
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-16 18:06:09 -0400

    Part-3-Solution-For-CollatzConjecture

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -2,20 +2,23 @@ package part1;
 
 public class SalaryCalculator {
     public static double calculateSalaryBeforeTaxes(double hoursPerMonth, double hourlyRate) {
-        double salaryBeforeTax = Math.ceil(hoursPerMonth * hourlyRate * 100) / 100;
+        double salaryBeforeTax1 = (hoursPerMonth * hourlyRate);
+        double salaryBeforeTax = Math.ceil(salaryBeforeTax1 * 100) / 100;
         return salaryBeforeTax;
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
-        double tax = hoursPerMonth * hourlyRate * 0.05 + 1562.00;
-        double salaryAfterTax1 = hoursPerMonth * hourlyRate - tax;
+        double salaryBeforeTax = Math.ceil(hoursPerMonth * hourlyRate * 100) / 100;
+        double tax = (salaryBeforeTax * 0.05) + 1562.00;
+        double salaryAfterTax1 = (hoursPerMonth * hourlyRate) - tax;
         double salaryAfterTax = Math.ceil(salaryAfterTax1 * 100) / 100;
         return salaryAfterTax;
-
     }
+
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(160, 10));
-        System.out.println(calculateSalaryAfterTaxes(160, 10));
+        System.out.println(calculateSalaryBeforeTaxes(40, 290.24));
+        System.out.println(calculateSalaryAfterTaxes(40, 290.24));
     }
+
 }
