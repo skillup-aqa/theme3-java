144a0a503694b32b8e1d9be7d29b0d092fd4715b
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-16 17:12:33 -0400

    Part-3-Solution-For-CollatzConjecture

--- src/main/java/part2/LeapYearChecker.java
+++ src/main/java/part2/LeapYearChecker.java
@@ -2,19 +2,24 @@ package part2;
 
 public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
-        boolean result = false;
+        boolean result = true;
 
-        if (year % 400 == 0) {
-            result = true;
-        } else if (year % 4 == 0 && year % 100 != 0) {
-            result = true;
+        if (year % 4 != 0) {
+            result = false;
+        } else if ((year % 400 != 0) && (year % 100 == 0)) {
+            result = false;
         }
-        return result;
+            return result;
+        }
+
+
+        public static void main (String[]args){
+            int year = 2020;
+            boolean result = isLeapYear(year);
+            System.out.println(result);
+        }
     }
 
-    public static void main(String[] args) {
-        int year = 2000;
-        boolean result = isLeapYear(year);
-        System.out.println(result);
-    }
-}
+
+
+
