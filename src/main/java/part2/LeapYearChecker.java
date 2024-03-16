e8866a41e69e4076ca237542088ab83a60a554f3
Slavik Aleksandruk <slavikmmk@gmail.com>
2024-03-16 16:47:31 -0400

    implementing part2 4th task

--- src/main/java/part2/LeapYearChecker.java
+++ src/main/java/part2/LeapYearChecker.java
@@ -3,13 +3,17 @@ package part2;
 public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
         // Read about leap years here: https://en.wikipedia.org/wiki/Leap_year
-        // TODO: Implement solution here
-        return false;
+
+        if (year % 4 == 0 || year % 100 ==0 && year % 400 != 0) { return true; }
+         else { return false; }
+
+
     }
 
     public static void main(String[] args) {
         int year = 2020;
         boolean result = isLeapYear(year);
         System.out.println(result);
+        System.out.println(year);
     }
 }
