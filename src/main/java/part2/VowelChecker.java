a96f6bf68458ed81075120af33d854cb0595700d
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-15 22:58:07 -0400

    Resolving VowelChecker

--- src/main/java/part2/VowelChecker.java
+++ src/main/java/part2/VowelChecker.java
@@ -5,7 +5,6 @@ public class VowelChecker {
         // Consider, that character is a latin lowercase letter.
         return "aeiou".indexOf(character) != -1;
     }
-
     public static void main(String[] args) {
         char character = 'o';
         boolean result = isVowel(character);
