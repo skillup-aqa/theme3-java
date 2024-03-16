4eb1b0bafa24105c3099a8aec4e493562ce87498
Tanya Mozkova <tmozkovaya@gmail.com>
2024-03-16 18:09:05 -0400

    Part-1-Salary-Calculation-fixed

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -17,8 +17,8 @@ public class SalaryCalculator {
 
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(40, 290.24));
-        System.out.println(calculateSalaryAfterTaxes(40, 290.24));
+        System.out.println(calculateSalaryBeforeTaxes(160, 10));
+        System.out.println(calculateSalaryAfterTaxes(160, 10));
     }
 
 }
