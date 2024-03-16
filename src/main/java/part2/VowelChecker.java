db834003f3f24b8e4bc56c5ad4d7f8938b42d53a
Ivan Senkiv <senkvvan@gmail.com>
2024-03-16 09:17:44 +0000

    "Done for LeapYearChecker "

--- src/main/java/part2/VowelChecker.java
+++ src/main/java/part2/VowelChecker.java
@@ -1,10 +1,10 @@
 package part2;
 
+
 public class VowelChecker {
     public static boolean isVowel(char character) {
         // Consider, that character is a latin lowercase letter.
-        // TODO: Implement solution here
-        return false;
+        return character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y';
     }
 
     public static void main(String[] args) {
