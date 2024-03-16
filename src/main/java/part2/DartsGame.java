53df69f282b81e90db0c1de4a488ca0f8ceb27cc
Slavik Aleksandruk <slavikmmk@gmail.com>
2024-03-16 10:32:01 -0400

    implementing part1's 6th task

--- src/main/java/part2/DartsGame.java
+++ src/main/java/part2/DartsGame.java
@@ -2,13 +2,21 @@ package part2;
 
 public class DartsGame {
     public static int getScore(double x, double y) {
-        // TODO: Implement solution here
-        return -1;
+        double hypotenuse = Math.sqrt(x * x + y * y);
+        if (hypotenuse > 10) {return 0;}      // if (hypotenuse <= 1) {
+        else if (hypotenuse > 5) {return 1;}  //     return 10;
+        else if (hypotenuse > 1) { return 5;} // } else if (hypotenuse <= 5) {
+        else { return 10;} //     return 5;
+                                             // } else if (hypotenuse <= 10) {
+                                             //     return 1;
+                                              // } else {
+                                               //     return 0;
+                                                // }
     }
 
     public static void main(String[] args) {
-        double x = 1.0;
-        double y = 1.0;
+        double x = 7;
+        double y = 7;
         int result = getScore(x, y);
         System.out.println(result);
     }
