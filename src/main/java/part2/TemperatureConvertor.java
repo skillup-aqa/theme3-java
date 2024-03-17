92ae5702b20aa42f954d286f2253d391ffb4cfc5
Slavik Aleksandruk <slavikmmk@gmail.com>
2024-03-17 01:35:29 -0400

    implementing part2 7th task

--- src/main/java/part2/TemperatureConvertor.java
+++ src/main/java/part2/TemperatureConvertor.java
@@ -5,16 +5,32 @@ import java.util.Scanner;
 public class TemperatureConvertor {
     public static double convertCelsiusToFahrenheit(double celsius) {
         // TODO: Implement solution here
-        return -1;
+        return celsius * 9 / 5 + 32;
     }
 
     public static double convertFahrenheitToCelsius(double fahrenheit) {
         // TODO: Implement solution here
-        return -1;
+        return (fahrenheit - 32) * 5 / 9;
     }
 
     public static void requestInputAndCalculate() {
-        // TODO: Put here the request for temperature and scale
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Enter numeric value of temp for С or F:");
+        double temperature = in.nextDouble();
+        System.out.println("choose C or F ");
+
+        String unit = in.next();
+        if (unit == "F") {
+            System.out.println("temp in C:" + convertFahrenheitToCelsius(temperature));
+        } else if (unit == "C") {
+            System.out.println("temp in F:" + convertCelsiusToFahrenheit(temperature));
+        } else {
+            System.out.println("");
+        }
+
+
+
+
     }
 
     public static void main(String[] args) {
