51b6b0bd1d24cec891b225675fb0e859423f5cf0
sacramentoview@gmail.com <sacramentoview@gmail.com>
2024-03-18 12:28:50 -0700

    Tetiana Nix - homework

--- src/main/java/part1/HoursToMinutesConvertor.java
+++ src/main/java/part1/HoursToMinutesConvertor.java
@@ -2,13 +2,22 @@ package part1;
 
 public class HoursToMinutesConvertor {
     public static int convertHoursToMinutes(int hours) {
-        // TODO: Implement solution here
-        return -1;
+
+
+        int M = hours * 60;
+
+        System.out.println("Minutes="+M);
+
+        return M;
     }
 
     public static int convertHoursToSeconds(int hours) {
-        // TODO: Implement solution here
-        return -1;
+
+
+        int M = hours * 60;
+        double S = M * 60;
+        System.out.println("Seconds="+S);
+        return (int) S;
     }
 
     public static void main(String[] args) {
