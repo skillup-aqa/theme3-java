fd46cda1dfc450b3a2005cdd0584507616799271
alina-wayne <malinka2012demochko@gmail.com>
2024-03-18 00:13:01 -0400

    Changes to main block 1 Implementing solution for calculateArithmeticMean Implementing C1 to F1 ( C = 0) Implementing C2 to F2 ( C = 100) Implementing C3 to F3 ( C = 0) Implementing division program Implementing HoursToMinutesAndSecondsConvertor Implementing PercentCalculator Implementing SalaryCalculator test commit Changing in ArithmeticMeanCalculator.java "double a" Rename HoursToMinutesAndSecondsConvertor.java class Update CelsiusToFahrenheitConvertor.java Updated DivisionWithRemainderCalculator.java Updating HoursToMinutesAndSecondsConverter.java Updating PercentCalculator.java Updating SalaryCalculator.java Implementing a program that checking numbers on "odd" or "even" Implementing method for MaxOfThreeChecker.java

--- src/main/java/part2/MaxOfThreeChecker.java
+++ src/main/java/part2/MaxOfThreeChecker.java
@@ -2,14 +2,20 @@ package part2;
 
 public class MaxOfThreeChecker {
     public static int maxOfThree(int number1, int number2, int number3) {
-        // TODO: Implement solution here
-        return -1;
+        int max = number1;
+        if (number2 > max) {
+            max = number2;
+        }
+        if (number3 > max) {
+            max = number3;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-        int number1 = 1;
-        int number2 = 2;
-        int number3 = 3;
+        int number1 = 12;
+        int number2 = 9;
+        int number3 = 4;
         int result = maxOfThree(number1, number2, number3);
         System.out.println(result);
     }
