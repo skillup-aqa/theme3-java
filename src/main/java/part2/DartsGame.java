5c237a334d65b83dc1516eb10bd2245468fe9000
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-17 21:49:32 -0400

    Changes to HW part 2 Darts Game

--- src/main/java/part2/DartsGame.java
+++ src/main/java/part2/DartsGame.java
@@ -3,16 +3,17 @@ package part2;
 public class DartsGame {
     public static int getScore(double x, double y) {
         double radius = Math.sqrt(x * x + y * y);
-       if (radius <= 1) {
-           return 10;
-       }else if (radius <= 5) {
-           return 5;
-       }else if (radius <= 10) {
-           return 1;
-       }else {
-           return 0;
-       }
-}
+        if (radius <= 1) {
+            return 10;
+        } else if (radius <= 5) {
+            return 5;
+        } else if (radius <= 10) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
         double x = 15.0;
         double y = 15.0;
