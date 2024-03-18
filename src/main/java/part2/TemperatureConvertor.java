306839bb020f9ec947ab65424cba4754debe76a6
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-18 08:55:18 -0400

    Changes to Temperature Convertor

--- src/main/java/part2/TemperatureConvertor.java
+++ src/main/java/part2/TemperatureConvertor.java
@@ -1,26 +1,33 @@
 package part2;
+
 import java.util.Scanner;
+
 public class TemperatureConvertor {
     public static double convertCelsiusToFahrenheit(double celsius) {
         return (celsius * 1.8) + 32;
     }
+
     public static double convertFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) / 1.8;
     }
+
     public static void requestInputAndCalculate() {
-    Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter temperature");
         double temperature = in.nextDouble();
 
-        System.out.println ("Enter type of temperature C oR F");
+        System.out.println("Enter type of temperature C or F");
         String type = in.next();
 
-        if (type == "C") {
-            System.out.println("Temperature in Fahrenheit" + convertCelsiusToFahrenheit(temperature));
+        if (type =="C") {
+            System.out.println("Temperature in Fahrenheit " + convertCelsiusToFahrenheit(temperature));
+        } else if (type =="F") {
+            System.out.println("Temperature in Celsius " + convertFahrenheitToCelsius(temperature));
         } else {
-            System.out.println("Temperature in Celsius" + convertFahrenheitToCelsius(temperature));
+            System.out.println("Incorrectly entered type");
         }
     }
+
     public static void main(String[] args) {
         requestInputAndCalculate();
     }
