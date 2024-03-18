7b1ffe6722ea686e0bd2a3dbfab542cd8322618d
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-17 20:48:55 -0400

    Changes to Celsius to Fahrenheit Convertor

--- src/main/java/part1/CelsiusToFahrenheitConvertor.java
+++ src/main/java/part1/CelsiusToFahrenheitConvertor.java
@@ -2,8 +2,9 @@ package part1;
 
 public class CelsiusToFahrenheitConvertor {
     public static double convertCelsiusToFahrenheit(double celsius) {
-        return (celsius * 9/5) + 32;
+        return (celsius * 9 / 5) + 32;
     }
+
     public static void main(String[] args) {
         System.out.println(convertCelsiusToFahrenheit(0)); // Expected output: 32.0
         System.out.println(convertCelsiusToFahrenheit(100)); // Expected output: 212.0
