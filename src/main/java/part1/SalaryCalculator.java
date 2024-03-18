c620db4fa947b8628a742595ae4aeade896d5d9d
Slavik Aleksandruk <slavikmmk@gmail.com>
2024-03-17 21:47:07 -0400

    implementing refraction of part1 6th task

--- src/main/java/part1/SalaryCalculator.java
+++ src/main/java/part1/SalaryCalculator.java
@@ -3,18 +3,19 @@ package part1;
 public class SalaryCalculator {
     public static double calculateSalaryBeforeTaxes(double hoursPerMonth, double hourlyRate) {
         double salary = hoursPerMonth * hourlyRate;
-        return Math.floor(salary);
+        return salary;
+
     }
 
     public static double calculateSalaryAfterTaxes(double hoursPerMonth, double hourlyRate) {
         double grossSalary = hoursPerMonth * hourlyRate;
         double percentageAmount = grossSalary * 0.95 - 1562;
         double SalaryAfterTaxes = grossSalary - percentageAmount;
-        return Math.floor(SalaryAfterTaxes);
+        return Math.ceil(SalaryAfterTaxes * 100) / 100.0;
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalaryBeforeTaxes(160, 10));
-        System.out.println(calculateSalaryAfterTaxes(160, 10));
+        System.out.println(calculateSalaryBeforeTaxes(160, 10.98));
+        System.out.println(calculateSalaryAfterTaxes(160, 10.11));
     }
 }
