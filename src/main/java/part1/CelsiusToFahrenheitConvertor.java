5c8b9da85300454e431b5d77483ab4f7fdf82cb6
alina-wayne <malinka2012demochko@gmail.com>
2024-03-18 22:57:10 -0400

    Changes to main block 1 Implementing solution for calculateArithmeticMean Implementing C1 to F1 ( C = 0) Implementing C2 to F2 ( C = 100) Implementing C3 to F3 ( C = 0) Implementing division program Implementing HoursToMinutesAndSecondsConvertor Implementing PercentCalculator Implementing SalaryCalculator test commit Changing in ArithmeticMeanCalculator.java "double a" Rename HoursToMinutesAndSecondsConvertor.java class Update CelsiusToFahrenheitConvertor.java Updated DivisionWithRemainderCalculator.java Updating HoursToMinutesAndSecondsConverter.java Updating PercentCalculator.java Updating SalaryCalculator.java Implementing a program that checking numbers on "odd" or "even" Implementing method for MaxOfThreeChecker.java Implementing solving method for LeapYearChecker Implementing code for VowelChecker.java Fixed idea J errors and re-trying Just commit Creating method for CelsiusToFahrenheitConvertor

--- src/main/java/part1/CelsiusToFahrenheitConvertor.java
+++ src/main/java/part1/CelsiusToFahrenheitConvertor.java
@@ -1,15 +1,15 @@
 package part1;
 public class CelsiusToFahrenheitConvertor {
 
-    public static double celsiusToFahrenheitConvertor(double celsius) {
+    public static double convertCelsiusToFahrenheit(double celsius) {
        double fahrenheit = (celsius * 9/5) + 32;
         return fahrenheit;
     }
 
     public static void main(String[] args) {
-        System.out.println(celsiusToFahrenheitConvertor(0)); // Expected output: 32.0
-        System.out.println(celsiusToFahrenheitConvertor(100)); // Expected output: 212.0
-        System.out.println(celsiusToFahrenheitConvertor(37)); // Expected output: 98.6
+        System.out.println(convertCelsiusToFahrenheit(2)); // Expected output: 32.0
+        System.out.println(convertCelsiusToFahrenheit(99)); // Expected output: 212.0
+        System.out.println(convertCelsiusToFahrenheit(40)); // Expected output: 98.6
     }
 }
 
