64d9997906d42304ea90b144ee29540df70bc1c2
Dima <korio455455@gmail.com>
2024-03-18 19:58:18 -0500

    Change Leap,Maxof three, Points

--- src/main/java/part2/PointsToMarksConvertor.java
+++ src/main/java/part2/PointsToMarksConvertor.java
@@ -4,7 +4,12 @@ public class PointsToMarksConvertor {
     public static char convertPointsToMarks(int points) {
         // Assume that points are always non-negative integers and less than 100
         // TODO: Implement solution here
-        return 'X';
+        if (points <= 100 && points >= 90) {return 'A'; }
+        else if (points <= 89 && points >= 85) {return 'B'; }
+        else if (points <= 84 && points >= 75) { return 'C'; }
+        else if (points <= 74 && points >= 60) { return 'D'; }
+        else return 'F';
+
     }
 
     public static void main(String[] args) {
