bdd98eaad63fbc059873b01361b8a6be01e5061c
Tetiana Yakymiuk <t882005@yahoo.com>
2024-03-18 23:19:02 -0400

    Changes to HW part 3 Math Calculator

--- src/main/java/part3/MathCalculator.java
+++ src/main/java/part3/MathCalculator.java
@@ -2,35 +2,64 @@ package part3;
 
 public class MathCalculator {
     public static int sumPositiveNumbersToN(int n) {
-        // Consider 0 as a positive number
-        // TODO: Implement solution here
-        return -1;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     public static int factorial(int n) {
-        // TODO: Implement solution here
-        return -1;
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 
     public static boolean isPrime(int n) {
         // Consider 1 and negative numbers as non-prime
-        // TODO: Implement solution here
-        return false;
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) ;
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean isArmstrongNumber(int n) {
-        // TODO: Implement solution here
-        return false;
+        int originalNumber = n;
+        int numberOfDigits = 0;
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += Math.pow(digit, numberOfDigits);
+            n /= 10;
+        }
+        return sum == originalNumber;
     }
 
     public static void printFibonacciNumbers(int n) {
-        // TODO: Implement solution here
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
     }
 
     public static void printDigitsReversed(int n) {
         // Use System.out.print() to print the digits without new line
-        // TODO: Implement solution here
-
+        while (n > 0) {
+            int digit = n % 10;
+            System.out.print("\n " + digit);
+            n /= 10;
+        }
     }
 
     public static void main(String[] args) {
@@ -38,7 +67,7 @@ public class MathCalculator {
         printDigitsReversed(12345);
 
         int n = 5;
-        System.out.println("Sum of positive numbers up to " + n + " is " + sumPositiveNumbersToN(n));
+        System.out.println("\nSum of positive numbers up to " + n + " is " + sumPositiveNumbersToN(n));
         System.out.println(n + " factorial is " + factorial(n));
         System.out.println(n + " is prime: " + isPrime(n));
         System.out.println(n + " is Armstrong number: " + isArmstrongNumber(n));
