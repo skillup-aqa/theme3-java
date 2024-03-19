40edb5a2b54af94fce758047991a3439ee08ac21
alina-wayne <malinka2012demochko@gmail.com>
2024-03-18 23:52:29 -0400

    Changes to main block 1 Implementing solution for calculateArithmeticMean Implementing C1 to F1 ( C = 0) Implementing C2 to F2 ( C = 100) Implementing C3 to F3 ( C = 0) Implementing division program Implementing HoursToMinutesAndSecondsConvertor Implementing PercentCalculator Implementing SalaryCalculator test commit Changing in ArithmeticMeanCalculator.java "double a" Rename HoursToMinutesAndSecondsConvertor.java class Update CelsiusToFahrenheitConvertor.java Updated DivisionWithRemainderCalculator.java Updating HoursToMinutesAndSecondsConverter.java Updating PercentCalculator.java Updating SalaryCalculator.java Implementing a program that checking numbers on "odd" or "even" Implementing method for MaxOfThreeChecker.java Implementing solving method for LeapYearChecker Implementing code for VowelChecker.java Fixed idea J errors and re-trying Just commit Creating method for CelsiusToFahrenheitConvertor Creating PointsToMarksConvertor for part 2

--- src/main/java/part2/PointsToMarksConvertor.java
+++ src/main/java/part2/PointsToMarksConvertor.java
@@ -1,15 +1,34 @@
 package part2;
 
+import java.util.Scanner;
+
 public class PointsToMarksConvertor {
-    public static char convertPointsToMarks(int points) {
-        // Assume that points are always non-negative integers and less than 100
-        // TODO: Implement solution here
-        return 'X';
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter student's points (0-100): ");
+        int mark = scanner.nextInt();
+
+        char letterMark = getLetterMark(mark);
+        System.out.println("Student's letter mark: " + letterMark);
+
+        scanner.close();
     }
 
-    public static void main(String[] args) {
-        int points = 90;
-        char result = convertPointsToMarks(points);
-        System.out.println(result);
+    public static char getLetterMark(int mark) {
+        if (mark >= 90 && mark <= 100) {
+            return 'A';
+        } else if (mark >= 85 && mark <= 89) {
+            return 'B';
+        } else if (mark >= 75 && mark <= 84) {
+            return 'C';
+        } else if (mark >= 60 && mark <= 74) {
+            return 'D';
+        } else {
+            return 'F';
+        }
     }
 }
+
+
+
