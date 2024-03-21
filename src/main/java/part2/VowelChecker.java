dcbad6f5eede566f451943513fd296e8c61b981e
Dima <korio455455@gmail.com>
2024-03-20 21:24:40 -0500

    Change Even , Temperature, vowel

--- src/main/java/part2/VowelChecker.java
+++ src/main/java/part2/VowelChecker.java
@@ -4,7 +4,18 @@ public class VowelChecker {
     public static boolean isVowel(char character) {
         // Consider, that character is a latin lowercase letter.
         // TODO: Implement solution here
-        return false;
+        switch (character) {
+            case 'a':
+            case 'e':
+            case 'y':
+            case 'u':
+            case 'i':
+            case 'o':
+                return true;
+            default:
+                return false;
+
+        }
     }
 
     public static void main(String[] args) {
