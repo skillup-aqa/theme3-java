52327e1fbcd357870b7a7272d2d58696b7898a7d
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-21 18:49:47 +0200

    Matrix resolving

--- src/main/java/part4/Matrix.java
+++ src/main/java/part4/Matrix.java
@@ -7,7 +7,48 @@ public class Matrix {
     }
 
     public static int[][] fillMatrixBySpiral(int size) {
-        // TODO: Implement solution here
-        return null;
+        int[][] matrixNatural = new int[size][size];
+        int natural = 1;
+        int top = 0, bottom = size - 1, left = 0, right = size - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrixNatural[top][i] = natural;
+                natural++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrixNatural[i][right] = natural;
+                natural++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                matrixNatural[bottom][i] = natural;
+                natural++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                matrixNatural[i][left] = natural;
+                natural++;
+            }
+            left++;
+        }
+
+
+        return matrixNatural;
+    }
+
+    public static void main(String[] args) {
+        String row = "";
+        int[][] matrix = fillMatrixBySpiral(5);
+        for (int i = 0; i < matrix.length; i++) {
+            row = "";
+            for (int j = 0; j < matrix.length; j++) {
+                row += matrix[i][j] + " ";
+            }
+            System.out.println(row);
+
+        }
+        System.out.println("value position");
+        System.out.println(matrix[1][3]);
     }
 }
