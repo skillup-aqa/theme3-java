591ecfe65f746b24de2f7ea565466a1052ca01d6
Sasha2511 <nedzelnytskyis@gmail.com>
2024-03-21 11:05:16 +0200

    getRandomCard resolving

--- src/main/java/part4/CardDeck.java
+++ src/main/java/part4/CardDeck.java
@@ -2,7 +2,20 @@ package part4;
 
 public class CardDeck {
     public static String getRandomCard() {
-        // TODO: Implement solution here
-        return null;
+        String[] suit = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] cardType = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                "Jack", "Queen", "King", "Ace"};
+        int i = (int) (Math.random() * suit.length);
+        int j = (int) (Math.random() * cardType.length);
+
+
+        return "Random card is: " + cardType[j] + " " + suit[i];
     }
+
+    public static void main(String[] args) {
+        System.out.println(getRandomCard());
+    }
+
+
 }
+
