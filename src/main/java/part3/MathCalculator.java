dac1c6f3932ec5a3e7f5d9219f2db938bfc5e43d
Dima <korio455455@gmail.com>
2024-04-02 14:39:29 -0500

    Change MathDigitreversed

--- src/main/java/part3/MathCalculator.java
+++ src/main/java/part3/MathCalculator.java
@@ -1,43 +1,92 @@
 package part3;
 
+
 public class MathCalculator {
     public static int sumPositiveNumbersToN(int n) {
         // Consider 0 as a positive number
         // TODO: Implement solution here
-        return -1;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+
+        }
+
+        return sum;
     }
 
     public static int factorial(int n) {
         // TODO: Implement solution here
-        return -1;
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+
+        }
+        return sum;
     }
 
     public static boolean isPrime(int n) {
         // Consider 1 and negative numbers as non-prime
         // TODO: Implement solution here
-        return false;
+        if (n <= 1)
+            return false;
+
+                for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+
     }
 
     public static boolean isArmstrongNumber(int n) {
         // TODO: Implement solution here
-        return false;
-    }
+        int Number = n;
+        int numDigits = String.valueOf(n).length();
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += Math.pow(digit, numDigits);
+            n /= 10;
+        }
+
+       return sum == Number;
+
+
+}
 
     public static void printFibonacciNumbers(int n) {
         // TODO: Implement solution here
-    }
+        int f = 0;
+        int s = 1;
+
+        for (int i = 2; i < n; i++) {
+            int num = f + s;
+
+            System.out.print(num + " ");
+            f = s;
+            s = num;
+        }
+        System.out.println("FibonacciNumbers" + " ");
+            }
+
+
 
     public static void printDigitsReversed(int n) {
         // Use System.out.print() to print the digits without new line
         // TODO: Implement solution here
-
+        while (n > 0) {
+            System.out.print(n % 10);
+            n /= 10;
+        }
+        System.out.println();
     }
-
     public static void main(String[] args) {
         printFibonacciNumbers(10);
         printDigitsReversed(12345);
 
-        int n = 5;
+        int n = 25;
         System.out.println("Sum of positive numbers up to " + n + " is " + sumPositiveNumbersToN(n));
         System.out.println(n + " factorial is " + factorial(n));
         System.out.println(n + " is prime: " + isPrime(n));
